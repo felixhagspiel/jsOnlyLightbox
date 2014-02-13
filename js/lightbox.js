7a96bedb9467cb568aee402b92137198f75bf4e6
Felix Hagspiel <mail@felixhagspiel.de>
2014-02-13 17:03:04 +0100

    added refresh-function; removed clickhandlerhelper from load-fnc

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,4 +1,11 @@
 function Lightbox () {
+	this.opt;
+	this.box;
+	this.wrapper;
+
+	var that = this,
+		template = '<span class="lightbox-close" id="lightbox-close">X</span><div class="lightbox-contentwrapper" id="lightbox-contentwrapper" ></div>';
+
 	/* Helpers */
 	var addEvent  = function(el,e,callback,val){
 		callback.bind(this,val);
@@ -22,11 +29,9 @@ function Lightbox () {
 	    if(!hasClass(el,className)) { el.className += ' '+className; }
 	    return el;
 	};
-	var that = this,
-		template = '<span class="lightbox-close" id="lightbox-close">X</span><div class="lightbox-contentwrapper" id="lightbox-contentwrapper" ></div>';
-	this.box;
-	this.wrapper;
+
 	this.load = function(opt) {
+		if(opt){this.opt = opt;}
 		if(opt && opt.boxId) {
 			// init lightbox on given element
 			this.box = document.getElementById(opt.boxId);
@@ -52,52 +57,19 @@ function Lightbox () {
 				that.closeLightbox();
 			});
 		}
-
-		this.closeLightbox = function() {
-			removeClass(that.box,'active');
-			removeClass(that.wrapper,'active');
-			document.getElementsByTagName('body')[0].setAttribute('style','overflow = auto');
-		};
-		this.openLightbox = function(img) {
-			if(!img){return false;}
-			if(typeof img === 'object') {
-				img = img;
-			}
-			else if (typeof img === 'string') {
-				var el = document.createElement('img');
-				el.setAttribute('src',img);
-				img = el;
-			}
-			else {
-				return false;
-			}
-			document.getElementsByTagName('body')[0].setAttribute('style','overflow = hidden');
-			this.box.setAttribute('style','padding-top: 0');
-			this.wrapper.innerHTML = '';
-			this.wrapper.appendChild(img.cloneNode(true));
-			addClass(this.box,'active');
-			this.box.setAttribute('style','padding-top: '+Math.floor(((window.innerHeight - this.wrapper.offsetHeight) /2))+'px');
-			var checkClassInt = setInterval(function(){
-				if(hasClass(that.box,'active'))
-				{
-					setTimeout(function(){
-						that.box.setAttribute('style','padding-top: '+Math.floor(((window.innerHeight - that.wrapper.offsetHeight) /2))+'px');
-						// prevent 'popup'-effect of image
-						setTimeout(function(){
-							addClass(that.wrapper,'active');
-						},10);
-					},10);
-					clearInterval(checkClassInt);
-				}
-			},10);
-		};
+		if(opt.responsive) {
+			console.info('set');
+			addEvent(window,'resize',function(e){
+				that.resize();
+				console.info('resized');
+			});
+		}
 		/* Find all thumbnails & add clickhandlers */
 		var images = document.getElementsByTagName('img');
 		var clckHlpr = function(i) {
 			addEvent(i,'click',function(e) {
 				if(this.getAttribute('data-lightbox') !== '') {
 					that.openLightbox(this.getAttribute('data-lightbox'));
-					console.log(this.getAttribute('data-lightbox'));
 				}
 				else {
 					that.openLightbox(this);
@@ -111,5 +83,54 @@ function Lightbox () {
 			}
 		}
 	};
+	this.closeLightbox = function() {
+		removeClass(that.box,'active');
+		removeClass(that.wrapper,'active');
+		document.getElementsByTagName('body')[0].setAttribute('style','overflow = auto');
+	};
+	this.resize = function() {
+		that.box.setAttribute('style','padding-top: '+Math.floor(((window.innerHeight - that.wrapper.offsetHeight) /2))+'px');
+	}
+	this.refresh = function(opt) {
+		if(opt) {
+			this.opt = opt;
+		}
+		this.load(this.opt);
+	}
+	this.openLightbox = function(img) {
+		if(!img){return false;}
+		if(typeof img === 'object') {
+			img = img;
+		}
+		else if (typeof img === 'string') {
+			var el = document.createElement('img');
+			el.setAttribute('src',img);
+			img = el;
+		}
+		else {
+			return false;
+		}
+		document.getElementsByTagName('body')[0].setAttribute('style','overflow = hidden');
+		this.box.setAttribute('style','padding-top: 0');
+		this.wrapper.innerHTML = '';
+		img = img.cloneNode(true);
+		img.setAttribute('class',' ');
+		this.wrapper.appendChild(img.cloneNode(true));
+		addClass(this.box,'active');
+		var checkClassInt = setInterval(function(){
+			if(hasClass(that.box,'active'))
+			{
+				setTimeout(function(){
+					that.resize();
+					// prevent 'popup'-effect of image
+					setTimeout(function(){
+						addClass(that.wrapper,'active');
+					},1);
+				},1);
+				clearInterval(checkClassInt);
+			}
+		},10);
+	};
+
 };
 
