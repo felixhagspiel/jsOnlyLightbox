85c1e14817c61a810507bd66b1f723b3de830c52
Felix Hagspiel <mail@felixhagspiel.de>
2014-02-14 03:26:57 +0100

    multiple IE8 Fixes&hacks

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,13 +1,15 @@
 
 function Lightbox () {
-	this.opt;
-	this.box;
-	this.wrapper
+	this.opt = false;
+	this.box = false;
+	this.wrapper = false;
 	var that = this,
-		template = '<span class="lightbox-close" id="lightbox-close">X</span><div class="lightbox-contentwrapper" id="lightbox-contentwrapper" ></div>';
+		template = '<span class="jslghtbx-close" id="jslghtbx-close">X</span><div class="jslghtbx-contentwrapper" id="jslghtbx-contentwrapper" ></div>';
 	/* Helpers */
+	var getHeight = function(){
+		return window.innerHeight || document.documentElement.offsetHeight;
+	}
 	var addEvent  = function(el,e,callback,val){
-		callback.bind(this,val);
 	    if (el.addEventListener) {
 	        el.addEventListener(e,callback, false);
 	    } else if (el.attachEvent) {
@@ -28,22 +30,45 @@ function Lightbox () {
 	    if(!hasClass(el,className)) { el.className += ' '+className; }
 	    return el;
 	};
-
+	var isset = function(obj) {
+		if(typeof obj != 'undefined'){return true;}
+		return false;
+	};
+	var objClone = function(obj){
+	    return eval(uneval(this));
+	};
+	var getAttr = function(obj,attr) {
+		if(!obj || typeof obj == undefined){return false;}
+		var ret;
+		if(obj.getAttribute){ret=obj.getAttribute(attr);}
+		else if(obj.getAttributeNode){ret=obj.getAttributeNode(attr).value;}
+		if(typeof ret != undefined && ret != ''){return ret;}
+		return false;
+	};
+	var hasAttr = function(obj,attr) {
+		if(!obj || typeof obj == undefined){return false;}
+		var ret;
+		if(obj.getAttribute){ret=obj.getAttribute(attr);}
+		else if(obj.getAttributeNode){ret=obj.getAttributeNode(attr).value;}
+		if(typeof ret != undefined){return true;}
+		return false;
+	};
+	/* Methods */
 	this.load = function(opt) {
 		if(opt){this.opt = opt;}
-		if(opt && opt.boxId) {
+		if(opt && opt.boxId && !this.box) {
 			// init lightbox on given element
 			this.box = document.getElementById(opt.boxId);
 			this.box.innerHTML = template;
 		}
-		else {
+		else if(!this.box) {
 			// create lightbox if no ID is given
 			var newEl = document.createElement('div');
-			newEl.setAttribute('class','lightbox');
+			newEl.setAttribute('class','jslghtbx');
 			this.box = newEl;
 			this.box.innerHTML = template;
 			document.getElementsByTagName('body')[0].appendChild(this.box);
-			this.wrapper = document.getElementById('lightbox-contentwrapper');
+			this.wrapper = document.getElementById('jslghtbx-contentwrapper');
 		}
 		if(opt && opt.closeId) {
 			// close lightbox on click on given element
@@ -51,12 +76,12 @@ function Lightbox () {
 				that.closeLightbox();
 			});
 		}
-		if(!opt.closeOnClick) {
+		if(opt && !opt.closeOnClick) {
 			addEvent(this.box,'click',function(e){
 				that.closeLightbox();
 			});
 		}
-		if(opt.responsive) {
+		if(opt && (opt.responsive || !isset(opt.responsive)) ) {
 			addEvent(window,'resize',function(e){
 				that.resize();
 			});
@@ -65,17 +90,19 @@ function Lightbox () {
 		var images = document.getElementsByTagName('img');
 		var clckHlpr = function(i) {
 			addEvent(i,'click',function(e) {
-				if(this.getAttribute('data-lightbox') !== '') {
-					that.openLightbox(this.getAttribute('data-lightbox'));
+				if(getAttr(i,'data-jslghtbx')) {
+					// image-source given
+					that.openLightbox(getAttr(i,'data-jslghtbx'));
 				}
 				else {
-					that.openLightbox(this);
+					// no image-source given
+					that.openLightbox(getAttr(i,'src'));
 				}
 			});
 		};
 		for(var i = 0; i < images.length; i++)
 		{
-			if(images[i].getAttribute('data-lightbox') !== null) {
+			if(hasAttr(images[i],'data-jslghtbx')) {
 				clckHlpr(images[i]);
 			}
 		}
@@ -86,7 +113,7 @@ function Lightbox () {
 		document.getElementsByTagName('body')[0].setAttribute('style','overflow = auto');
 	};
 	this.resize = function() {
-		that.box.setAttribute('style','padding-top: '+Math.floor(((window.innerHeight - that.wrapper.offsetHeight) /2))+'px');
+		that.box.setAttribute('style','padding-top:'+((getHeight() - that.wrapper.offsetHeight) /2)+'px');
 	}
 	this.refresh = function(opt) {
 		if(opt) {
@@ -94,25 +121,14 @@ function Lightbox () {
 		}
 		this.load(this.opt);
 	}
-	this.openLightbox = function(img) {
-		if(!img){return false;}
-		if(typeof img === 'object') {
-			img = img;
-		}
-		else if (typeof img === 'string') {
-			var el = document.createElement('img');
-			el.setAttribute('src',img);
-			img = el;
-		}
-		else {
-			return false;
-		}
+	this.openLightbox = function(src) {
+		if(!src){return false;}
+		var img = document.createElement('img');
+		img.setAttribute('src',src);
 		document.getElementsByTagName('body')[0].setAttribute('style','overflow = hidden');
 		this.box.setAttribute('style','padding-top: 0');
 		this.wrapper.innerHTML = '';
-		img = img.cloneNode(true);
-		img.setAttribute('class',' ');
-		this.wrapper.appendChild(img.cloneNode(true));
+		this.wrapper.appendChild(img);
 		addClass(this.box,'active');
 		var checkClassInt = setInterval(function(){
 			if(hasClass(that.box,'active'))
@@ -123,7 +139,7 @@ function Lightbox () {
 					setTimeout(function(){
 						addClass(that.wrapper,'active');
 					},1);
-				},1);
+				},100);
 				clearInterval(checkClassInt);
 			}
 		},10);
