ac4042c619c6372bae823ea0e2803c9fdaee3792
Christopher Train <ctrain@pcs-publishing.com>
2015-10-27 08:59:26 +0000

    Now checks to see if lightbox is open before stopping the propagation of the event

--- js/lightbox.js
+++ js/lightbox.js
@@ -479,23 +479,29 @@ function Lightbox() {
         if (CTX.opt.keyControls) {
             // show next img on right cursor
             addEvent(document, 'keydown', function (e) {
-                stopPropagation(e); // prevent closing of lightbox
-                if (isOpen && e.keyCode === 39) {
-                    CTX.next();
+                if (isOpen) {
+                    stopPropagation(e); // prevent closing of lightbox
+                    if (e.keyCode === 39) {
+                        CTX.next();
+                    }
                 }
             }, false);
             // show prev img on left cursor
             addEvent(document, 'keydown', function (e) {
-                stopPropagation(e); // prevent closing of lightbox
-                if (isOpen && e.keyCode === 37) {
-                    CTX.prev();
+                if (isOpen) {
+                    stopPropagation(e); // prevent closing of lightbox
+                    if (e.keyCode === 37) {
+                        CTX.next();
+                    }
                 }
             }, false);
             // close box on escape
             addEvent(document, 'keydown', function (e) {
-                stopPropagation(e); // prevent closing of lightbox
-                if (isOpen && e.keyCode === 27) {
-                    CTX.close();
+                if (isOpen) {
+                    stopPropagation(e); // prevent closing of lightbox
+                    if (e.keyCode === 27) {
+                        CTX.next();
+                    }
                 }
             }, false);
         }
